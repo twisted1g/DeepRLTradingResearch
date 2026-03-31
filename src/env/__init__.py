75a80964762fa1cf359eaa9ebc5dae98a6c60560
David <zaharandavid1@gmail.com>
2026-03-31 13:25:52 +0300

    chore

--- src/env/__init__.py
+++ src/env/__init__.py
@@ -1,3 +1,11 @@
 from .trading_env import MyTradingEnv
+from .rewards.drawdown_reward import DrawdownAwareReward
+from .rewards.return_reward import ReturnReward
+from .rewards.sharpe_reward import SharpeReward
 
-__all__ = ["MyTradingEnv"]
+__all__ = [
+    "DrawdownAwareReward",
+    "MyTradingEnv",
+    "ReturnReward",
+    "SharpeReward",
+]
